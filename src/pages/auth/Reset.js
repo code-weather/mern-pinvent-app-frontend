0f078e492025664bdd8283d17f5eb2d43089dbd9
Jameson Wang <jamesonw10@gmail.com>
2023-03-03 15:55:09 -0500

    reset password page JSX

--- src/pages/auth/Reset.js
+++ src/pages/auth/Reset.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
+import styles from './auth.module.scss';
+import { MdPassword } from 'react-icons/md';
+import Card from '../../components/card/Card';
 
 const Reset = () => {
   return (
-    <div>Reset</div>
-  )
-}
+    <div className={`container ${styles.auth}`}>
+      <Card>
+        <div className={styles.form}>
+          <div className="--flex-center">
+            <MdPassword size={35} color="#999" />
+          </div>
+          <h2>Reset Password</h2>
 
-export default Reset
+          <form>
+            <input
+              type="password"
+              placeholder="New Password"
+              required
+              name="password"
+            />
+            <input
+              type="password"
+              placeholder="Confirm New Password"
+              required
+              name="password"
+            />
+
+            <button className="--btn --btn-primary --btn-block" type="submit">
+              Reset Password
+            </button>
+            <div className={styles.links}>
+              <p>
+                <Link to="/">- Home</Link>
+              </p>
+              <p>
+                <Link to="/register">- Login</Link>
+              </p>
+            </div>
+          </form>
+        </div>
+      </Card>
+    </div>
+  );
+};
+
+export default Reset;
