2a65903eed15c33532c84bc80f980eb3d5190e90
Jameson Wang <jamesonw10@gmail.com>
2023-03-02 23:52:49 -0500

    Login page JSX

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
+import styles from './auth.module.scss';
+import { BiLogIn } from 'react-icons/bi';
+import Card from '../../components/card/Card';
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className={`container ${styles.auth}`}>
+      <Card>
+        <div className={styles.form}>
+          <div className="--flex-center">
+            <BiLogIn size={35} color="#999" />
+          </div>
+          <h2>Login</h2>
 
-export default Login
+          <form>
+            <input type="text" placeholder="Email" required name="email" />
+            <input
+              type="text"
+              placeholder="Password"
+              required
+              name="password"
+            />
+            <button className="--btn --btn-primary --btn-block" type="submit">
+              Login
+            </button>
+          </form>
+          <Link to="/forgot">Forgot Password</Link>
+          <span className={styles.register}>
+            <Link to="/">Home</Link>
+            <p>&nbsp; Don't have an account? &nbsp;</p>
+            <Link to="/register">Register</Link>
+          </span>
+        </div>
+      </Card>
+    </div>
+  );
+};
+
+export default Login;
